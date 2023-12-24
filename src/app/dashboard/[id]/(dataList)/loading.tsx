940a45cfaf45b9b91638d92d5aaf50c2b1397f71
Sherif Awad <eng.sherifawad@gmail.com>
2023-12-24 14:23:14 +0200

    style: add skeleton to table mobile view

--- src/app/dashboard/[id]/(dataList)/loading.tsx
+++ src/app/dashboard/[id]/(dataList)/loading.tsx
@@ -9,9 +9,11 @@ import {
 } from "@/components/ui/table";
 
 export const TableSkeleton = () => (
-  <div className="max-w-4xl mx-auto mb-8">
-    <div className="space-y-4">
-      <div className="rounded-md border">
+  <div className="max-w-4xl mx-auto my-8">
+    <div className="space-y-4 mx-3 xs:mx-0">
+      <Skeleton className="h-8 w-full" />
+
+      <div className="rounded-md border hidden md:block">
         <Table>
           <TableHeader>
             <TableRow>
@@ -32,6 +34,11 @@ export const TableSkeleton = () => (
             ))}
           </TableBody>
         </Table>
+      </div>
+      <div className="grid grid-rows-5 gap-3  md:hidden">
+        {[...new Array(5)].map((_, idx) => (
+          <Skeleton key={idx} className="h-24 w-full" />
+        ))}
       </div>
       <Skeleton className="h-8 w-full" />
     </div>
