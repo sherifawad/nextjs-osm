570e00245cbe54ae889f5fc07f932c4aa164e727
Sherif Awad <eng.sherifawad@gmail.com>
2023-12-24 12:53:24 +0200

    refactor: loading table skeleton splite component

--- src/app/dashboard/[id]/(dataList)/loading.tsx
+++ src/app/dashboard/[id]/(dataList)/loading.tsx
@@ -8,36 +8,38 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-function LoadingPage() {
-  return (
-    <div className="max-w-4xl mx-auto mb-8">
-      <div className="space-y-4">
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead colSpan={5}>
-                  <Skeleton className="h-8 " />
-                </TableHead>
+export const TableSkeleton = () => (
+  <div className="max-w-4xl mx-auto mb-8">
+    <div className="space-y-4">
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead colSpan={5}>
+                <Skeleton className="h-8 " />
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {[...new Array(5)].map((_, idx) => (
+              <TableRow key={idx}>
+                {[...new Array(5)].map((_, idx) => (
+                  <TableCell key={idx}>
+                    <Skeleton className="h-6 w-full" />
+                  </TableCell>
+                ))}
               </TableRow>
-            </TableHeader>
-            <TableBody>
-              {[...new Array(5)].map((_, idx) => (
-                <TableRow key={idx}>
-                  {[...new Array(5)].map((_, idx) => (
-                    <TableCell key={idx}>
-                      <Skeleton className="h-6 w-full" />
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </div>
-        <Skeleton className="h-8 w-full" />
+            ))}
+          </TableBody>
+        </Table>
       </div>
+      <Skeleton className="h-8 w-full" />
     </div>
-  );
+  </div>
+);
+
+function LoadingPage() {
+  return <TableSkeleton />;
 }
 
 export default LoadingPage;
